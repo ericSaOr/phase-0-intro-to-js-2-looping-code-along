ca990f5c2a8c723e08dbc92eafe21f9d5f5503a5
Eric Sanchez <ericfsod@gmail.com>
2021-06-19 17:41:56 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+//   } 
+
+//   const gifts = ["teddy bear", "drone", "doll"];
+
+//   function wrapGifts(gifts) {
+//       for (let i = 0; i < gifts.length; i++){
+//       console.log('Wrapped ${gifts[i]} and added a bow!');
+//       debugger;
+//       }
+
+//       return gifts;
+//   }
+
+//   wrapGifts(gifts);
+
+const arr = ['Lisa', 'Kaitlin', 'Jan'];
+const str = 'suprise';
+
+function writeCards (arr, str){
+    const messageArray = [];
+    for(let i = 0; i < arr.length; i++){
+       messageArray.push(`Thank you, ${arr[i]}, for the wonderful ${str} gift!`);
+        
+    }
+    return messageArray;
+
+}
+// writeCards(["Eric", "Alex", "Bob"], "birthday");
+
+function countDown (num = 10){
+    while( num >= 0){
+        console.log(num--);
+    }
+}
+
